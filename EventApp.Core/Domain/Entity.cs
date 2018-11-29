d9b007556d70e27292357aaf03534e7539b7789a
Dawid Sikorski <dsikorski40@gmail.com>
2018-11-29 17:36:09 +0100

    created base abscract entity class - Entity.cs

---
+++ EventApp.Core/Domain/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventApp.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            SetCreatedAt();
+        }
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+            SetCreatedAt();
+        }
+
+        protected void SetCreatedAt()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
