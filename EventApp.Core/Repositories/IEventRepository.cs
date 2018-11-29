6a45bc2a2d0931bfd0a0cb115bcec47c73c8724c
Dawid Sikorski <dsikorski40@gmail.com>
2018-11-29 17:51:34 +0100

    created interfaces - IEventRepository and IUserRepository

---
+++ EventApp.Core/Repositories/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventApp.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApp.Core.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<Event> GetAsync(Guid id);
+        Task<Event> GetAsync(string name);
+        Task<IEquatable<Event>> BrowseAsync();
+        Task AddAsync(Domain.Event @event);
+        Task UpdateAsync(Domain.Event @event);
+        Task DeleteAsync(Domain.Event @event);
+    }
+}
---
+++ EventApp.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventApp.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApp.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task<IEquatable<User>> BrowseAsync();
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
