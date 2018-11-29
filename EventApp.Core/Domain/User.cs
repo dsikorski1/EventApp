e105bafb31174fcd1338db9f1b7a2723cc70d324
Dawid Sikorski <dsikorski40@gmail.com>
2018-11-29 17:38:37 +0100

    created User entity

---
+++ EventApp.Core/Domain/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventApp.Core.Domain
+{
+    class User : Entity
+    {
+        public string Username { get; protected set; }
+        public string Email { get; protected set; }
+        public string Firstname { get; protected set; }
+        public string Lastname { get; protected set; }
+        public string Password { get; protected set; }
+        public string Role { get; protected set; }
+
+        public User() : base()
+        {
+        }
+
+        public User(Guid guid) : base(guid)
+        {
+        }
+    }
+}
