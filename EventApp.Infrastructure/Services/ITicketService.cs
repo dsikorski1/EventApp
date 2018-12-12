22503c51e5d47234fcd091a9b8b57b934d1eed78
Dawid Sikorski <dsikorski40@gmail.com>
2018-12-12 18:17:57 +0100

    added method GetTicketOrFailAsync in RepositoryExtension, created ITicketService and implements this in TicketService

---
+++ EventApp.Infrastructure/Services/ITicketService.cs
@@ -0,0 +1,15 @@
+﻿using EventApp.Infrastructure.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApp.Infrastructure.Services
+{
+    public interface ITicketService
+    {
+        Task<TicketDto> GetAsync(Guid userId, Guid eventId, Guid ticketId);
+        Task PurchaseAsync(Guid userId, Guid eventId, int amount);
+        Task CancelAsync(Guid userId, Guid eventId, int amount);
+    }
+}
---
+++ EventApp.Infrastructure/Services/TicketService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EventApp.Core.Repositories;
+using EventApp.Infrastructure.DTO;
+using EventApp.Infrastructure.Extensions;
+
+namespace EventApp.Infrastructure.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IEventRepository eventRepository;
+        private IMapper mapper;
+
+        public TicketService(IUserRepository userRepository, IEventRepository eventRepository, IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.eventRepository = eventRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<TicketDto> GetAsync(Guid userId, Guid eventId, Guid ticketId)
+        {
+            var user = await userRepository.GetOrFailAsync(userId);
+            var ticket = await eventRepository.GetTicketOrFailAsync(eventId, ticketId);
+
+            return mapper.Map<TicketDto>(ticket);
+        }
+
+        public async Task PurchaseAsync(Guid userId, Guid eventId, int amount)
+        {
+            var user = await userRepository.GetOrFailAsync(userId);
+            var @event = await eventRepository.GetOrFailAsync(eventId);
+
+            @event.PurchaseTickets(user, amount);
+            await eventRepository.UpdateAsync(@event);
+
+            await Task.CompletedTask;
+        }
+
+        public async Task CancelAsync(Guid userId, Guid eventId, int amount)
+        {
+            var user = await userRepository.GetOrFailAsync(userId);
+            var @event = await eventRepository.GetOrFailAsync(eventId);
+
+            @event.CancelTickets(user, amount);
+            await eventRepository.UpdateAsync(@event);
+      
+            await Task.CompletedTask;
+        }
+    }
+}
